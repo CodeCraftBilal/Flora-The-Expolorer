9c805f5f04a9d3644e6a9585c540f92d78586410
Muhammad Bilal Khan <bilalkhan751150@gmail.com>
2026-02-13 23:48:20 +0500

    created scene controller

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,16 +1,35 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static SceneController instance;
+    private Stack<string> sceneHistory = new Stack<string>();
+
+    void Awake()
     {
-        
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            sceneHistory.Push(SceneManager.GetActiveScene().name);
+        } else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadScene(string sceneName)
     {
-        
+        sceneHistory.Push(sceneName);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void GoBack()
+    {
+        sceneHistory.Pop();
+        string previouseScene = sceneHistory.Peek();
+        SceneManager.LoadScene(previouseScene);
     }
 }
