f772cc2c5374a7b21bace4aba5de84d4c0ba6e46
Muhammad Bilal Khan <bilalkhan751150@gmail.com>
2026-02-14 20:15:10 +0500

    created fruits prefabs

--- Assets/Scripts/FruitDroperScript.cs
+++ Assets/Scripts/FruitDroperScript.cs
@@ -3,11 +3,20 @@ using UnityEngine;
 
 public class FruitDroperScript : MonoBehaviour
 {
-    public GameObject applePrefab;
+    public GameObject[] fruits;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Instantiate(applePrefab, new Vector3(0f,6.10f,0f), quaternion.identity);
+        InvokeRepeating("dropFruits", 0.1f, 1);
+    }
+
+    void dropFruits()
+    { 
+        int randomIndex = UnityEngine.Random.Range(1, fruits.Length);
+
+        Instantiate(fruits[randomIndex], new Vector3(UnityEngine.Random.Range(-12.5f, 12.5f),6.10f,0f), quaternion.identity);
+        
+        
     }
 
     // Update is called once per frame
