98b50a97edb4e4aa1e4592d4899ca2d3462c8c66
Muhammad Bilal Khan <bilalkhan751150@gmail.com>
2026-03-16 13:44:39 +0500

    create slide animation

--- Assets/Scripts/ScoreScript.cs
+++ Assets/Scripts/ScoreScript.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,10 +10,22 @@ public class ScoreScript : MonoBehaviour
     public GameObject GameOverPannel;
     public Text inGameScoreText;
     public Text afterGameScoreText;
+    public Text TargetFruitText;
+    public GameObject targetPannel;
+    public float targetDisplayTime = 3f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Time.timeScale = 1;
+        Debug.Log("timescale: " + Time.timeScale);
+        TargetTextShow();
         GameOverPannel.SetActive(false);
+        StartCoroutine(HideTargetText());
+    }
+
+    private void TargetTextShow()
+    {
+        TargetFruitText.text = "Eat " + DataManager.instance.scoreToWin + DataManager.instance.targetFruitName;
     }
 
     // Update is called once per frame
@@ -20,27 +34,42 @@ public class ScoreScript : MonoBehaviour
         score = DataManager.instance.currentScore;
         gameOverScore = score;
         inGameScoreText.text = DataManager.instance.targetFruitName + " : " + score;
-        if(score == DataManager.instance.scoreToWin)
+        if (score == DataManager.instance.scoreToWin)
         {
             GameOverPannel.SetActive(true);
             afterGameScoreText.text = "You Win";
             inGameScoreText.enabled = false;
+            DataManager.instance.currentScore = 0;
             Time.timeScale = 0;
         }
     }
 
+    IEnumerator HideTargetText()
+    {
+        yield return new WaitForSeconds(targetDisplayTime);
+        targetPannel.SetActive(false);
+    }
+
+    private object WaitForSeconds()
+    {
+        throw new NotImplementedException();
+    }
+
     public void SelectLevel()
     {
+        Time.timeScale = 1;
         SceneController.instance.LoadScene("LevelScene");
     }
 
     public void Restart()
     {
+        Time.timeScale = 1;
         SceneController.instance.LoadScene("GamePlayScene");
     }
 
     public void Home()
     {
+        Time.timeScale = 1;
         SceneController.instance.LoadScene("HomeScene");
     }
 }
